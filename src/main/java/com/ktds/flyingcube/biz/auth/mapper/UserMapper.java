3acfaaf96c50dcb1d7733a79fecd66e7c24dc2ad
Lee Yu Pyeong <stoneberg73@gmail.com>
2021-02-02 17:49:29 +0900

    mapping ignore

--- src/main/java/com/ktds/flyingcube/biz/auth/mapper/UserMapper.java
+++ src/main/java/com/ktds/flyingcube/biz/auth/mapper/UserMapper.java
@@ -4,10 +4,12 @@ import com.ktds.flyingcube.biz.auth.domain.User;
 import com.ktds.flyingcube.biz.auth.dto.UserRes.UserDto;
 import com.ktds.flyingcube.config.MapStructMapperConfig;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapStructMapperConfig.class)
 public interface UserMapper {
 
+    @Mapping(target = "roles", ignore = true)
     UserDto toDto(User user);
 
 }
