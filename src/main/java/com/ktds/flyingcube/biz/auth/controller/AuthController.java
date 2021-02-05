23365d35b261cfa28dcf4a7281336ba608705b99
Lee Yu Pyeong <stoneberg73@gmail.com>
2021-02-05 09:44:35 +0900

    jwtResponse

--- src/main/java/com/ktds/flyingcube/biz/auth/controller/AuthController.java
+++ src/main/java/com/ktds/flyingcube/biz/auth/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,8 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -45,14 +42,9 @@ public class AuthController {
     public ResponseEntity<?> signin(@Valid @RequestBody LoginDto loginDto) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
-
+        // set userinfo
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-
         String accessToken = jwtUtils.generateJwt(userDetails);
         String refreshToken = jwtUtils.generateRefreshJwt(userDetails);
         // set refresh token to redis
@@ -62,10 +54,7 @@ public class AuthController {
                 .type(JwtUtils.BEARER_TOKEN)
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
-                .id(userDetails.getId())
-                .username(userDetails.getUsername())
-                .email(userDetails.getEmail())
-                .roles(roles)
+                .user(userDetails)
                 .build();
 
         return ResponseEntity.ok(jwtResponse);
--- src/main/java/com/ktds/flyingcube/common/response/JwtResponse.java
+++ src/main/java/com/ktds/flyingcube/common/response/JwtResponse.java
@@ -1,11 +1,10 @@
 package com.ktds.flyingcube.common.response;
 
+import com.ktds.flyingcube.config.security.service.UserDetailsImpl;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -15,13 +14,11 @@ public class JwtResponse {
 	private String accessToken;
 	private String refreshToken;
 	private String type;
-	private Integer id;
-	private String username;
-	private String email;
-	private List<String> roles;
+	private UserDetailsImpl user;
 
-	public JwtResponse(String accessToken, String refreshToken) {
+	public JwtResponse(String accessToken, String refreshToken, UserDetailsImpl user) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
+		this.user = user;
 	}
 }
