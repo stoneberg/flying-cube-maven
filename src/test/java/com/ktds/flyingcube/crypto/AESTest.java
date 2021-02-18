8a0c1dc11f3728b1e7dcab7c89b228084440a488
Lee YuPyeong <stoneberg73@gmail.com>
2021-02-18 09:47:42 +0900

    aes test

---
+++ src/test/java/com/ktds/flyingcube/crypto/AESTest.java
@@ -0,0 +1,27 @@
+package com.ktds.flyingcube.crypto;
+
+import com.ktds.flyingcube.common.utils.AESUtils;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class AESTest {
+
+    private static final Logger log = LoggerFactory.getLogger(AESTest.class);
+
+    @Autowired
+    private AESUtils aesUtils;
+
+    @Test
+    void runDecryptTest() {
+        String chipherText = "U2FsdGVkX19C760rM7qZjGgE7lIJxIUdFEnqn3+2x9MNnhisL56b1eUsDiUfXKsL";
+        final String decrypt = aesUtils.decrypt(chipherText);
+        log.info("@decrypt text=====>{}", decrypt);
+        assertThat(decrypt).isEqualTo("where is my hat?");
+    }
+}
